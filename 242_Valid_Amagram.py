579d4c75c86cc6820f52dd457e2d84f0fe8be4db
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2023-07-24 14:10:49 +0200

    did some basic algos to learn stuff

---
+++ 242_Valid_Amagram.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/valid-anagram/
+# Algo: Hash Table
+# Too slow
+# class Solution:
+# def isAnagram(self, s: str, t: str) -> bool:
+#     if sorted(s) == sorted(t):
+#         return True
+#     else:
+#         return False
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_map, t_map = {}, {}
+        for n in range(len(s)):
+            s_map[s[n]] = s_map.get(s[n], 0) + 1
+            t_map[t[n]] = t_map.get(t[n], 0) + 1
+        for c in s_map:
+            if c not in t_map or s_map[c] != t_map[c]:
+                return False
+        return True
